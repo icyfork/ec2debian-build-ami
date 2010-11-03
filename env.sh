ab01ce42b35157397ac356ffcf7141961c785467
icy <kyanh@viettug.org>
2010-11-03 15:25:34 +0700

    Fix typo

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 export EC2_CERT=/home/pi/ec2_keys/cert-ACU6FFQI6BWLRBSSFU3QCDQVXRL5EANM.pem
 export EC2_PRIVATE_KEY=/home/pi/ec2_keys/pk-ACU6FFQI6BWLRBSSFU3QCDQVXRL5EANM.pem
 export AWS_ACCESS_KEY_ID="$(cat $HOME/.awssecret |sed -n 1p)"
-export AWS_SECRET_ACCESS_KEY_ID="$(cat $HOME/.awssecret |sed -n 2p)"
+export AWS_SECRET_ACCESS_KEY="$(cat $HOME/.awssecret |sed -n 2p)"
 export AWS_USER_ID="858768646416"
 
 export EC2_HOME=/home/pi/df/ec2-api-tools-1.3-57419/
