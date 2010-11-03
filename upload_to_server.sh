a5e2705b417e0f2d82cd6d6b6ee763e04f3d7c88
icy <kyanh@viettug.org>
2010-11-03 15:58:46 +0700

    Fix typo

--- upload_to_server.sh
+++ upload_to_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 _server_ip="10.0.0.62"
 echo >&2 "Upload scripts and keys to server (deb64, $_server_ip) to build"
-scp *.sh awssecret $_server_pi:~/$(basename $PWD)/
+echo scp *.sh awssecret $_server_ip:~/$(basename $PWD)/
