31fac171d89cd0acf9ce120014cddb2bae4281c3
icy <kyanh@viettug.org>
2010-11-03 16:03:54 +0700

    Force remove old link to codename. Upload also main script

--- upload_to_server.sh
+++ upload_to_server.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 _server_ip="10.0.0.62"
 echo >&2 "Upload scripts and keys to server (deb64, $_server_ip) to build"
-echo scp *.sh awssecret $_server_ip:~/$(basename $PWD)/
+scp \
+  ec2debian-build-ami \
+  *.sh awssecret \
+  $_server_ip:~/$(basename $PWD)/
